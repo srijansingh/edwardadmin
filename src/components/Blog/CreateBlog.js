2e8094bb7a8279354618e7f6c26dbeab95c8c538
Srijan Singh <srijan.singh.1232@gmail.com>
2020-07-03 00:21:49 +0530

    added tinymce

--- src/components/Blog/CreateBlog.js
+++ src/components/Blog/CreateBlog.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { withStyles } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
+import { Typography, Button, CircularProgress } from "@material-ui/core";
+import CloudUploadIcon from "@material-ui/icons/CloudUploadRounded";
+import { Editor } from '@tinymce/tinymce-react';
 
-
+import "./CreateBlog.css";
 
 const styles = (theme) => ({
     root: {
@@ -17,6 +19,48 @@ const styles = (theme) => ({
   
 
 class CreateBlog extends Component {
+    state = {
+            thumblink:null,
+            imagelink: null,
+            category:null,  
+            title:null,
+            content:null,
+            isLoading:false,
+            selected:false
+        }
+        handleEditorChange = (content, editor) => {
+            this.setState({
+                content : content
+            })
+          }
+
+    handleContent =() =>{
+        this.setState({isLoading : true})
+        fetch('https://edserver.herokuapp.com/api/blog', {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(this.state)
+            }).then(result => {
+                result.json().then(response => {
+                    console.log(response)
+                    this.setState({
+                        isLoading: false
+                    })
+                    alert('Post Added Successfully')
+                    window.location.reload(false);
+                    
+                })
+            }).catch(err => {
+                alert("Something went wrong")
+                this.setState({
+                    isLoading: false,
+                });
+            });  
+         }
+
     render() {
         return (
             <div>
@@ -30,7 +74,60 @@ class CreateBlog extends Component {
                     </div>
 
                     <div>
-                        
+                        <div className="blog-container">
+                            <div className="blog-table-container">
+                                <table>
+                                    <tr>
+                                        <td style={{width:'150px'}}>Enter Title</td>
+                                        <td><input type="text" onChange={(event)=>{this.setState({title:event.target.value})}}/></td>
+                                    </tr>
+
+                                    <tr>
+                                        <td style={{width:'150px'}}>Enter Category</td>
+                                        <td><input type="text" onChange={(event)=>{this.setState({category:event.target.value})}}/></td>
+                                    </tr>
+
+                                    <tr>
+                                        <td style={{width:'150px'}}>Thumbnail Link</td>
+                                        <td><input type="link" onChange={(event)=>{this.setState({imagelink:event.target.value})}}/></td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td style={{width:'150px'}}>Content</td>
+                                        <td>
+                                        <Editor
+                                            initialValue="<p>This is content area.</p>"
+                                            apiKey="1hqpmn9thspuot5ho3g7bruc34rlizbcc5fl3rmb5bcgm8x4"
+                                            init={{
+                                            height: 500,
+                                            menubar:true,
+                                            plugins: [
+                                                'advlist autolink lists link emoticons  image  charmap print preview anchor hr',
+                                                'searchreplace visualblocks code  fullscreen fullpage',
+                                                'insertdatetime media table paste code help wordcount'
+                                            ],
+                                            toolbar:
+                                                'preview |fullscreen fullpage| undo redo |formatselect |  fontselect |  fontsizeselect| link image media|sizeselect | bold italic |emoticons backcolor forecolor|hr  alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | visualblocks | help'
+                                            }}
+                                            onEditorChange={this.handleEditorChange}
+                                        />
+                                        </td>
+                                    </tr>
+                              
+                                    <tr>
+                                        <td></td>
+                                        <td >
+                                            {
+                                                this.state.isLoading ?
+                                                <Button style={{background:'white', color:'blue'}} startIcon={<CircularProgress/>} color="primary" >Uploading...</Button>
+                                                :
+                                                <Button style={{background:'blue', color:'white'}} startIcon={<CloudUploadIcon />} color="primary" onClick={this.handleContent}>Upload Now</Button>
+                                            }
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Gallery/ViewGallery.js
+++ src/components/Gallery/ViewGallery.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { withStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 
-
+import "./ViewGallery.css";
 
 const styles = (theme) => ({
     root: {
@@ -17,7 +17,51 @@ const styles = (theme) => ({
   
 
 class ViewGallery extends Component {
+    constructor(){
+        super();
+        this.state = {
+            isLoading : false,
+            category:[]
+        }
+    }
+
+    componentDidMount(){
+        this.setState({
+            isLoading:true
+        });
+
+        fetch('https://edserver.herokuapp.com/api/gallery', {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                Authorization: 'Bearer '+this.props.token
+            }
+        })
+        .then(res => {
+            if(res.status !==200){
+                throw new Error('Failed to fetch the product')
+            }
+            return res.json()
+        }).then(response => {
+            console.log(response.data)
+            this.setState({
+                category:response.data
+            }) 
+        })
+                
+    }
+
     render() {
+        const category = this.state.category.map((list, index) => {
+            return (
+                <div key={index} className="gallery-card">
+                    <div className="gallery-image">
+                        <img src={list.imagelink} alt={list.title+" edgav"}/>
+                    </div>
+                </div>
+            )
+        })
         return (
             <div>
                 <div>
@@ -29,8 +73,10 @@ class ViewGallery extends Component {
 
                     </div>
 
-                    <div>
-                        
+                    <div style={{padding:'1rem'}}>
+                       <div className="gallery-container">
+                            {category}
+                       </div>
                     </div>
                 </div>
             </div>
